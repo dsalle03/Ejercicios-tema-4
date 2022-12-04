ef8c5d0ee83af7fbd91206fadb21537201358de0
dsalle03 <dsalle2003@gmail.com>
2022-12-04 23:48:52 +0100

    7

--- Ejercicio4.py
+++ Ejercicio4.py
@@ -9,6 +9,7 @@ def Newton_Raphson():
     def first_derivative(function, point, dx=1e-5):
         f_x1 = function(point)
         f_x2 = function(point+dx)
+        first_derivative()
         return (f_x2-f_x1)/dx
 
 
